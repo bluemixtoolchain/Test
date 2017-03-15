0ebc841257cda976b9522a57504828aaaf9da26c
bluemixtoolchain <bluemixtoolchain@trash-mail.com>
2017-03-15 15:34:03 +0100

    asdf

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.ReadKey();
+            Console.ReadKey(); //Comment
 
         }
     }
